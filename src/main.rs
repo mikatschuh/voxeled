5cd8cab903b34d9eba5933ee9925ba469d66c50c
Mika Aßmus <mika.assmus@gmail.com>
2025-01-24 23:51:20 +0100

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut num = 10;
+
+    for _ in 0..10 {
+        println!("num: {}", num);
+
+        num -= 1;
+
+        sleep(Duration::from_secs(1))
+    }
 }
